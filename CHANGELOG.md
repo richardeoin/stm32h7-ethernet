46ece28871cfa05c9f92208ef1a1669bdb13cda2
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2020-09-09 23:45:37 +0200

    Depreciate crate in favour of stm32h7xx-hal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # Changelog
 
-## [Unreleased]
+## [v0.3.0] 2020-09-09
+
+This crate has been replaced by [Ethernet support within
+stm32h7xx-hal](https://docs.rs/stm32h7xx-hal/latest/stm32h7xx_hal/ethernet/index.html).
 
 ## [v0.2.0] 2020-07-05
 
@@ -16,6 +19,6 @@
 * Tidy unecessary brackets
 * Started Changelog
 
-[Unreleased]: https://github.com/richardeoin/stm32h7-ethernet/compare/v0.2.0...HEAD
+[v0.3.0]: https://github.com/richardeoin/stm32h7-ethernet/compare/v0.2.0...v0.3.0
 [v0.2.0]: https://github.com/richardeoin/stm32h7-ethernet/compare/v0.1.1...v0.2.0
 [v0.1.1]: https://github.com/richardeoin/stm32h7-ethernet/compare/v0.1.0...v0.1.1
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,5 @@
-//! This crate implements a [smoltcp][] device interface `phy::Device` for
-//! the STM32H7 series of microcontrollers.
-//!
-//! Multiple PHYs are supported:
-//! - SMSC LAN8742a
-//! - Micrel KSZ8081R
-//!
-//! To build this crate, a device feature of [stm32h7xx-hal][] must be
-//! selected. For example:
-//!
-//! ```text
-//! cargo build  --features stm32h743v
-//! ```
-//!
-//! When using this crate as a dependency, it re-exports the device
-//! features so you can specify them in Cargo.toml
-//!
-//! ```text
-//! stm32h7-ethernet = { version = 0.4.0, features = ["stm32h743v"] }
-//! ```
-//!
-//! Specifing device features is not nessesary if an identical version of
-//! stm32h7xx_hal is in use somewhere else in your dependency tree. In this
-//! case cargo unions the feature flags.
-//!
-//! # Hardware Examples
-//!
-//! ## STM32H747I-DISCO
-//!
-//! Targeting the STM32H747I-DISCO evaluation board from ST.
-//!
-//! *Note:* Close solder jumper SB8!
+//! ## This crate has been replaced by [Ethernet support within
+//! stm32h7xx-hal](https://docs.rs/stm32h7xx-hal/latest/stm32h7xx_hal/ethernet/index.html).
 //!
 //! ## License
 //!
